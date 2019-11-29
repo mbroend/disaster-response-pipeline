56647bf553a18bacc0995ee1a7d43abb0cf52fc9
Mathias Brønd Sørensen <mathias.b.sorensen@gmail.com>
2019-11-29 21:21:16 +0100

    Finished ML pipeline and uncommented model part in the app

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,61 @@
+# import libraries
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.externals import joblib
+from sklearn.metrics import classification_report
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql('SELECT * FROM Messages', engine)
+    
+    X = df['message']
+    Y = df.drop(columns=['id','message','original','genre'])
+    category_names = Y.columns.tolist()
+    return  X, Y, category_names 
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    model = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=100)))
+                    ])
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    y_pred = pd.DataFrame(y_pred,columns = Y_test.columns)
+    for col in Y_test.columns:
+        print('-----------------------{}-----------------------'.format(col))
+        print(classification_report(Y_test[col],y_pred[col], 
+                                    labels = [0,1], 
+                                    target_names = ['No','Yes']))
 
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath,  compress = 3)
 
 
 def main():
